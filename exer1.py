f83649afdf055ddb06d2178cebd787ccec191cd0
Pedrozukk <pedrohenriquevince@gmail.com>
2026-05-06 22:03:11 -0300

    Add files via upload

---
+++ exer1.py
@@ -0,0 +1,10 @@
+def acao_semaforo(cor: str):
+    cor = cor.lower()
+    if cor == 'vermelho':
+        return 'Pare'
+    elif cor == 'amarelo':
+        return 'Atenção'
+    elif cor == 'verde':
+        return 'Siga'
+    else:
+        return 'Cor inválida'
